ac51c4c442cba497f5909bcfdcab08ecd1b23d98
nnnkit <iankit17@gmail.com>
2019-05-11 15:16:04 +0530

    add hero section

--- src/components/mainHero.js
+++ src/components/mainHero.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Img from "gatsby-image";
+import { css } from "@emotion/core";
+import hero from "./../images/hero.svg";
 
 export default () => (
   <div>
-    <section>
-      <img src="" alt="" />
-      <h1>Gatsby Start</h1>
-      <h3>Let's make gatsby more accessable with themes! 🍻</h3>
+    <section className="hero__section">
+      <div>
+        <h1
+          css={css`
+            font-size: 40px;
+            font-weight: 100;
+          `}
+        >
+          Blazing Fast Website In Few Lines Of Code With Gatsy Themes
+        </h1>
+        <p css={css``}>Let's make gatsby more accessable with themes! 🍻</p>
+      </div>
+      <img src={hero} alt="" />
     </section>
   </div>
 );
